5f131faa1e2b55a5ec699dda4e4d052a92401362
wong2 <wonderfuly@gmail.com>
2023-10-30 19:22:16 +0800

    Fix ClientWindowOrigin setting

--- shell/content.js
+++
@@ -1,3 +0,0 @@
-const origin = `chrome-extension://${chrome.runtime.id}`;
-window.postMessage({ action: "setClientWindowOrigin", origin }, "*");
-console.debug("postMessage setClientWindowOrigin", origin);
--- shell/inject.js
+++ shell/inject.js
@@ -1,7 +1,18 @@
-window.addEventListener("message", (event) => {
-  const message = event.data;
-  if (message.action === "setClientWindowOrigin") {
-    console.debug("setClientWindowOrigin", message.origin);
-    window.Underside.ClientWindowOrigin = message.origin;
-  }
-});
+(function () {
+  const extensionId =
+    new URL(location.href).searchParams.get("extension") || "ncjedehfkpnliaafimjhdjjeggmfmlgf";
+  const origin = `chrome-extension://${extensionId}`;
+
+  let udsShellConfig = {};
+  Object.defineProperty(window, "_udsShellConfig", {
+    enumerable: true,
+    get() {
+      return udsShellConfig;
+    },
+    set(value) {
+      console.debug("set _udsShellConfig", value);
+      udsShellConfig = value;
+      udsShellConfig.ClientWindowOrigin = origin;
+    },
+  });
+})();
